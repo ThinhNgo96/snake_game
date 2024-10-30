fa99290d55161738df00cd01625dbce65ba1c2a0
Thinh Ngo <24730068@ms.uit.edu.vn>
2024-10-30 21:03:41 +0700

    change hash func to avoid hash collision

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ struct pair_hash {
     std::size_t operator()(const std::pair<T1, T2>& pair) const {
         auto hash1 = std::hash<T1>{}(pair.first);
         auto hash2 = std::hash<T2>{}(pair.second);
-        return hash1 ^ (hash2 << 1);  // Combine the hashes
+        return hash1 + 0x9e3779b9 + (hash2 << 6) + (hash2 >> 2);
     }
 };
 unordered_set<pair<int, int>, pair_hash> snakeOccupied;
